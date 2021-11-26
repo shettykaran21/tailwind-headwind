c9f51928cbc060eecb5b383339e4a6d8f5059f70
Karan Shetty <shettykaran2110@gmail.com>
2021-11-26 19:06:44 +0530

    Sidebar done

---
+++ src/components/Sidebar.js
@@ -0,0 +1,18 @@
+import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
+import { FaFire, FaPoo } from 'react-icons/fa';
+
+import SidebarIcon from './SidebarIcon';
+
+const Sidebar = () => {
+  return (
+    <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
+      <SidebarIcon icon={<FaFire size="28" />} />
+      <SidebarIcon icon={<BsPlus size="32" />} />
+      <SidebarIcon icon={<BsFillLightningFill size="20" />} />
+      <SidebarIcon icon={<FaPoo size="20" />} />
+      <SidebarIcon icon={<BsGearFill size="22" />} />
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/components/SidebarIcon.js
@@ -0,0 +1,10 @@
+const SidebarIcon = ({ icon, text = 'tooltip 💡' }) => {
+  return (
+    <div className="sidebar-icon group">
+      {icon}
+      <span className="sidebar-tooltip group-hover:scale-100">{text}</span>
+    </div>
+  );
+};
+
+export default SidebarIcon;
